c69702c0dd494f32de7d060393ffe2e873a1329d
Ruma Das <rumaadl16@gmail.com>
2020-07-15 00:44:02 +0930

    adding navebar page name

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,43 +5,37 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" to="/">
-        Pupster
-      </a>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              to="/discover"
-              className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
-            >
-              Discover
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              to="/search"
-              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
-            >
-              Search
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <nav class="navbar navbar-light navbar-expand-md py-3 fixed-top">
+    <div class="container" >
+        <a href="" class="navbar-brand">
+
+            <h3 class="d-inline align-middle"></h3>
+        </a>
+        <button class="navbar-toggler navbar-toggler-right" data-toggle="collapse" data-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#skill">skills</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#portfolio">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</nav>
   );
 }
 
