d5ce9d7ee52e1d48a4dc8fb425eaeb3e1a8a2147
Ruma Das <rumaadl16@gmail.com>
2020-07-15 01:03:30 +0930

    adding footer in projects

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,18 @@ import "./style.css";
 
 const  Footer = () => {
   return (
-    <footer className="footer">
-      <span>Pupster 2017</span>
-    </footer>
+    <footer id="footer-main" className="footer_style" >
+    <div className="container">
+        <div className="row">
+            <div className="col-12">
+
+                <span >Design By: <a href="#" target="_blank" >Ruma</a>
+
+                </span>
+            </div>
+        </div>
+    </div>
+</footer>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,31 +5,31 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav class="navbar navbar-light navbar-expand-md py-3 fixed-top">
-    <div class="container" >
-        <a href="" class="navbar-brand">
+    <nav className="navbar navbar-light navbar-expand-md py-3 fixed-top">
+    <div className="container" >
+        <a href="" className="navbar-brand">
 
-            <h3 class="d-inline align-middle"></h3>
+            <h3 className="d-inline align-middle"></h3>
         </a>
-        <button class="navbar-toggler navbar-toggler-right" data-toggle="collapse" data-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler navbar-toggler-right" data-toggle="collapse" data-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#home">Home</a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <a className="nav-link" href="#home">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#about">About</a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#about">About</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#skill">skills</a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#skill">skills</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#portfolio">Portfolio</a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#portfolio">Portfolio</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link " href="#contact">Contact</a>
+                <li className="nav-item">
+                    <a className="nav-link " href="#contact">Contact</a>
                 </li>
             </ul>
         </div>
