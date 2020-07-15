2d3bfda25bfbd2c3028848974d1c34f7fd97dac2
Ruma Das <rumaadl16@gmail.com>
2020-07-16 08:31:52 +0930

    adding 4th projects in portfolio

--- src/components/PortfolioText/index.js
+++ src/components/PortfolioText/index.js
@@ -3,6 +3,7 @@ import LionAPP from '../../images/Lion_app.png';
 import Suicide from '../../images/project_1.png';
 import Heroku from '../../images/icons8-heroku-48.png';
 import Prtfolio from '../../images/portfolio.png';
+import Weather from '../../images/weather_ dashboard.png';
 
 import "./style.css";
 
@@ -59,6 +60,22 @@ const index = () =>{
             </div>
         </div>
                 </div>
+                <div className="row pt-0 pt-md-4 ">
+                <div className="col-md-4">
+                    <div className="itemBox">
+                        <a href="https://rumardas.github.io/weather_dashboard/" target="_blank">
+                            <img src={Weather} alt="Nature" className="img-fluid"/>
+                        </a>
+                        <div className="box">
+                            <h3 className="text-info ">Weather Dashboard</h3>
+                            <p>Responsive website design using HTML5, CSS,BootStrap, Javascript,Jquery, Ajex</p>
+                            <a href="https://github.com/RumaRDas/weather_dashboard.git" target="_blank">
+                                <i className="fa fa-github text-info icon"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                </div> 
                 </div>
                
 
